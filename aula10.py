03be6588b53d4a462d39a27c0bef4037c90375c7
Gustavo Campos <115758013+gugacmp@users.noreply.github.com>
2023-08-30 13:45:45 -0300

    Hub Comerce

---
+++ aula10.py
@@ -0,0 +1,22 @@
+import time
+
+pedido = int(input('Informe o Pedido : '))
+
+class Gerenciamento():
+    def pendente():
+        print('Pendente')
+    def atualizando():
+        print('Atualizando')
+    def cancelado():
+        print('Cancelado')
+    def autorizado():
+        print('Autorizado')
+
+class Processamento():
+    Gerenciamento.atualizando()
+    
+
+print('|----------HUB COMERCE----------|')
+print('|--------PEDIDO ECOMERCE--------|')
+
+        
